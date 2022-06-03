9237992d9edcfa65736363f24c6864885ce3c655
Enes ÇEB <enesceb@hotmail.com>
2022-06-03 13:54:49 +0300

    styled components and created array

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
-import './App.css';
-import ExpenseItem from './components/ExpenseItem';
+import "./App.css";
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    { id:1, title: "Car Insurance", amount: "450,21$", date: new Date(2021, 8, 28) },
+    { id:2, title: "Electricity", amount: "230,99$", date: new Date(2021, 2, 8) },
+    { id:3, title: "Ethernet", amount: "78,49$", date: new Date(2021, 1, 18) },
+    { id:4, title: "Kitchen", amount: "29,99$", date: new Date(2021, 1, 18) },
+  ];
+
   return (
-    <div >
+    <div>
       <div className="App-header">
-     <h2 >Getting Started</h2>
-     <ExpenseItem></ExpenseItem>
-     </div>
+        <h2>Getting Started</h2>
+        <ExpenseItem
+        title ={expenses[0].title}
+        amount={expenses[0].amount}
+        date = {expenses[0].date}
+        />
+           <ExpenseItem
+        title ={expenses[1].title}
+        amount={expenses[1].amount}
+        date = {expenses[1].date}
+        />
+           <ExpenseItem
+        title ={expenses[2].title}
+        amount={expenses[2].amount}
+        date = {expenses[2].date}
+        />
+           <ExpenseItem
+        title ={expenses[3].title}
+        amount={expenses[3].amount}
+        date = {expenses[3].date}
+        />
+      
+      </div>
     </div>
   );
 }
