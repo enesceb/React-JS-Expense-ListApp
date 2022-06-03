cad4edc83445eae6996a483381ce6902f179ccf3
Enes ÇEB <enesceb@hotmail.com>
2022-06-03 17:46:57 +0300

    date formating and converting to component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
   const expenses = [
-    { id:1, title: "Car Insurance", amount: "450,21$", date: new Date(2021, 8, 28) },
-    { id:2, title: "Electricity", amount: "230,99$", date: new Date(2021, 2, 8) },
-    { id:3, title: "Ethernet", amount: "78,49$", date: new Date(2021, 1, 18) },
-    { id:4, title: "Kitchen", amount: "29,99$", date: new Date(2021, 1, 18) },
+    { id:1, title: "Car Insurance", amount: "450,21$", date: new Date(2021, 11, 4) },
+    { id:2, title: "Electricity", amount: "230,99$", date: new Date(2022, 6, 5) },
+    { id:3, title: "Ethernet", amount: "78,49$", date: new Date(2022, 7, 21) },
+    { id:4, title: "Kitchen", amount: "29,99$", date: new Date(2022, 8, 1) },
   ];
 
   return (
