09571ebdf14ed29bc0c74fb44c488f20a58e267b
Enes ÇEB <enesceb@hotmail.com>
2022-06-10 16:26:06 +0300

    reStruct components

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -4,6 +4,7 @@ import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import "./Expenses.css";
 import ExpenseFilter from "./ExpenseFilter";
+import ExpensesList from "./ExpensesList";
 
 const Expenses = (props) => {
   const [filtredYear, setFiltredYear] = useState("2020");
@@ -15,19 +16,7 @@ const Expenses = (props) => {
   const filtredExpenses = props.items.filter(expense => {
     return expense.date.getFullYear().toString() === filtredYear;
   });
-
-  let expensesContent = <p>No Expenses Found</p>;
-
-  if(filtredExpenses.length > 0 ){
-    expensesContent = filtredExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ))
-  }  
+ 
 
   return (
     <div>
@@ -36,7 +25,7 @@ const Expenses = (props) => {
           selected={filtredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {expensesContent}
+       <ExpensesList items={filtredExpenses}/>
       </Card>
     </div>
   );
